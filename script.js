8ff0d85f7523dee592f2df2326005ce8af5662c4
Ivo Nelissen <ivo@ivo.gg>
2018-12-10 21:42:32 +0100

    Changed countdown timer

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 let timerTarget = new Date("14 Dec 2018 18:00:00 PST");
-let timerContainer = document.querySelector("#countdownContent");
+let dayCount = document.querySelector("#countDays");
+let hourCount = document.querySelector("#countHours");
+let minuteCount = document.querySelector("#countMinutes");
+let secondCount = document.querySelector("#countSeconds");
+
 function leadingZero(number) {
   return ("0" + number).slice(-2);
 }
 function updateTimer() {
-  let diff = new Date(timerTarget - Date.now());
-  if (diff.getDate() > 1) {
-    timerContainer.innerText = `${diff.getDate() - 1} days`;
+  let nDiff = timerTarget - Date.now();
+  if(nDiff >= 0) {
+    let diff = new Date(nDiff);
+    dayCount.innerText = leadingZero(diff.getDate() - 1);
+    hourCount.innerText = leadingZero(diff.getHours() - 1);
+    minuteCount.innerText = leadingZero(diff.getMinutes());
+    secondCount.innerText = leadingZero(diff.getSeconds());
   } else {
-    timerContainer.innerText = `${leadingZero(diff.getHours())}:${leadingZero(
-      diff.getMinutes()
-    )}:${leadingZero(diff.getSeconds())}`;
+    document.querySelector("#countdown").style.display = "none";
   }
 }
 setInterval(updateTimer, 1000);
